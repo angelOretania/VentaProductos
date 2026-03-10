7b7cf27e8cf2cf1e80bd69d70f9b8d3c5f065064
Angel Tobaruela Baños <atobban3007@g.educaand.es>
2026-03-10 01:10:20 +0100

    Versión 2: cálculo de descuentos

--- src/com/empresa/ventas/Venta.java
+++ src/com/empresa/ventas/Venta.java
@@ -1,7 +1,13 @@
 package com.empresa.ventas;
 
 public class Venta {
+
     public double calcularTotal(Producto producto, int cantidad) {
         return producto.getPrecio() * cantidad;
+    }
+
+    public double calcularTotalConDescuento(Producto producto, int cantidad, double descuento) {
+        double total = calcularTotal(producto, cantidad);
+        return total - (total * descuento);
     }
 }
